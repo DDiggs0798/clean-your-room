7c386e76f3a24f2fa91a752e54d1e2770884e4be
Dominique Diggs <dominiquediggs@dominiques-air.worcester.local>
2019-09-23 22:08:08 -0400

    Cleaned up

--- pricing.js
+++ pricing.js
@@ -1,23 +1,29 @@
+function calculateVolLife (coverageLevels,product) {
+  var price = 0;
+  for (var i = 0; i < coverageLevels.length; i++) {
+    var coverageAmount = coverageLevels[i].coverage
+
+    price += (coverageAmount / product.cost.costDivisor) * product.cost.price
+  }
+
+  if (product.employerContribution.mode === 'dollar') {
+    price = price - product.employerContribution.contribution
+  } else {
+    dollarsOff = price * (product.employerContribution.contribution / 100)
+    price = price - dollarsOff
+  }
+
+  return parseInt(price * 100) / 100
+}
+
+
 module.exports.calculateProductPrice = function (product, employee, coverageLevels) {
   var price = 0
   var dollarsOff = 0
 
   switch (product.type) {
     case 'volLife':
-      for (var i = 0; i < coverageLevels.length; i++) {
-        var coverageAmount = coverageLevels[i].coverage
-
-        price += (coverageAmount / product.cost.costDivisor) * product.cost.price
-      }
-
-      if (product.employerContribution.mode === 'dollar') {
-        price = price - product.employerContribution.contribution
-      } else {
-        dollarsOff = price * (product.employerContribution.contribution / 100)
-        price = price - dollarsOff
-      }
-
-      return parseInt(price * 100) / 100
+        return calculateVolLife (coverageLevels, product);
     case 'ltd':
       var salaryPercentage = product.coveragePercentage / 100
 
